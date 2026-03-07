d4e6a6e5d4ee58d9936c79fe7ae85a1fee03a017
LAB13_USER ON WS35 <L13Y17W35@LabHCI.net>
2026-03-07 15:52:01 +0800

    Initial commit: Setup Spring Boot backend for AquaHaven

---
+++ backend/aquahaven/src/main/java/edu/cit/poliquit/aquahaven/AquaHavenApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.poliquit.aquahaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaHavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquaHavenApplication.class, args);
+	}
+
+}
---
+++ backend/aquahaven/src/test/java/edu/cit/poliquit/aquahaven/AquaHavenApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.poliquit.aquahaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaHavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
