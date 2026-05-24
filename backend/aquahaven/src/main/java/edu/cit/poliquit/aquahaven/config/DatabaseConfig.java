271a2de1efe5d6f3e9409bf704465b1c2eded175
Keint Warren <keintwarren.poliquit@cit.edu>
2026-05-25 00:43:57 +0800

    Fix DatabaseConfig: properly parse Render DATABASE_URL into jdbcUrl, username, password

--- backend/aquahaven/src/main/java/edu/cit/poliquit/aquahaven/config/DatabaseConfig.java
+++ backend/aquahaven/src/main/java/edu/cit/poliquit/aquahaven/config/DatabaseConfig.java
@@ -1,11 +1,15 @@
 package edu.cit.poliquit.aquahaven.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class DatabaseConfig {
@@ -13,17 +17,38 @@ public class DatabaseConfig {
     @Value("${DATABASE_URL:}")
     private String databaseUrl;
 
+    @Value("${spring.datasource.username:postgres}")
+    private String defaultUsername;
+
+    @Value("${spring.datasource.password:}")
+    private String defaultPassword;
+
     @Bean
-    public DataSource dataSource() {
+    @Primary
+    public DataSource dataSource() throws URISyntaxException {
+        HikariDataSource dataSource = new HikariDataSource();
+
         if (databaseUrl != null && !databaseUrl.isEmpty()) {
-            String jdbcUrl = databaseUrl;
-            if (jdbcUrl.startsWith("postgresql://")) {
-                jdbcUrl = "jdbc:" + jdbcUrl;
-            }
-            return DataSourceBuilder.create()
-                    .url(jdbcUrl)
-                    .build();
+            URI dbUri = new URI(databaseUrl);
+
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String jdbcUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+            dataSource.setJdbcUrl(jdbcUrl);
+            dataSource.setUsername(username);
+            dataSource.setPassword(password);
+        } else {
+            dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/aquahaven_db");
+            dataSource.setUsername(defaultUsername);
+            dataSource.setPassword(defaultPassword);
         }
-        return DataSourceBuilder.create().build();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setMinimumIdle(2);
+        dataSource.setConnectionTimeout(30000);
+
+        return dataSource;
     }
 }
